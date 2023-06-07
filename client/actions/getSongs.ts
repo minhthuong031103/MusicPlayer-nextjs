16995da81bdf5be05ac0faf7ba2fa88929d489aa
minhthuong031103 <nominh67@gmail.com>
2023-06-07 21:54:12 +0700

    fix header not found

--- client/actions/getSongs.ts
+++ client/actions/getSongs.ts
@@ -2,7 +2,7 @@
 
 import { Song } from '@/types';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { header, cookies } from 'next/headers';
+import { cookies } from 'next/headers';
 
 const getSongs = async function (): Promise<Song[]> {
   const supabase = createServerComponentClient({
